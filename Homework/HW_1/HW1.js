bee42251d5cf2bedc5bbb008ba7b8e74b6806619
OliynykA <123013256+OliynykA@users.noreply.github.com>
2023-03-29 17:07:25 +0300

    Delete Homework/HW_1 directory

--- Homework/HW_1/HW1.js
+++
@@ -1,7 +0,0 @@
-{ // Расчет количества багов в коде *по методике Bill Gates
-    let numCodeStr = prompt("Введите количество строк кода"); //ввод количества строк кода пользователем.
-    let debugCodeFault = +numCodeStr; //вычисление количества ошибок при отладке каждой строки кода. 
-    let noDebugCodeFault = Number(numCodeStr)**2; //вычисление количества ошибок без отладки кода.
-    let resultDebugCodeNum = alert(debugCodeFault); //вывод результата для кода с отладкой.
-    let resultNoDebugCodeNum = alert(noDebugCodeFault); //вывод результата для кода без отладки.
-}
